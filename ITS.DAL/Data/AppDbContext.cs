02c188c0a5c93b0c6727e92a3beeb22017edf0dc
karinanikolova <lianna_kn@yahoo.com>
2025-05-21 21:03:04 +0300

    Modified AppDbContext by adding Comments DbSet and applying more configuration for entities to the ModelBuilder.

--- ITS.DAL/Data/AppDbContext.cs
+++ ITS.DAL/Data/AppDbContext.cs
@@ -17,20 +17,22 @@ namespace ITS.DAL.Data
 		public DbSet<Ticket> Tickets { get; set; }
 		public DbSet<Category> Categories { get; set; }
 		public DbSet<Comment> Comments { get; set; }
+		public DbSet<Department> Departments { get; set; }
 
 		protected override void OnModelCreating(ModelBuilder builder)
 		{
 			base.OnModelCreating(builder);
 
+			builder.ApplyConfiguration(new DepartmentConfiguration());
 			builder.ApplyConfiguration(new UserConfiguration());
 			builder.ApplyConfiguration(new UserClaimsConfiguration());
+			builder.ApplyConfiguration(new RoleConfiguration());
+			builder.ApplyConfiguration(new UserRolesConfiguration());
+			builder.ApplyConfiguration(new CategoryConfiguration());
 			builder.ApplyConfiguration(new TicketConfiguration());
 			builder.ApplyConfiguration(new CommentConfiguration());
-			builder.ApplyConfiguration(new CategoryConfiguration());
 
 			// Specifying Identity entities table names
-			builder.Entity<ApplicationRole>().ToTable("Roles");
-			builder.Entity<IdentityUserRole<Guid>>().ToTable("UserRoles");
 			builder.Entity<IdentityUserLogin<Guid>>().ToTable("UserLogins");
 			builder.Entity<IdentityRoleClaim<Guid>>().ToTable("RoleClaims");
 			builder.Entity<IdentityUserToken<Guid>>().ToTable("UserTokens");
