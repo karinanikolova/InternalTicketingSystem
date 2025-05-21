90d5d3c15fb96d654e0d9aa69bf86cbe233684e5
karinanikolova <lianna_kn@yahoo.com>
2025-05-21 20:47:59 +0300

    Added more properties to ApplicationUser.

--- ITS.DAL/Data/Models/ApplicationUser.cs
+++ ITS.DAL/Data/Models/ApplicationUser.cs
@@ -1,9 +1,29 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static ITS.DAL.Constants.DataConstants.ApplicationUser;
+using static ITS.DAL.Constants.DataConstants.Department;
 
 namespace ITS.DAL.Data.Models
 {
 	public class ApplicationUser : IdentityUser<Guid>
 	{
-		
+		[Required]
+		[MaxLength(UserFirstNameMaxLength)]
+		[PersonalData]
+		public string FirstName { get; set; }
+
+		[Required]
+		[MaxLength(UserLastNameMaxLength)]
+		[PersonalData]
+		public string LastName { get; set; }
+
+		[Required]
+		[MaxLength(DepartmentNameMaxLength)]
+		[PersonalData]
+		public Guid DepartmentId { get; set; }
+
+		[ForeignKey(nameof(DepartmentId))]
+		public Department Department {  get; set; }
 	}
 }
