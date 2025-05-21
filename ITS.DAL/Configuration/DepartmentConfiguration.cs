65e94bc2b1a6fcd2d8940b2f862d50a04d6490d0
karinanikolova <lianna_kn@yahoo.com>
2025-05-21 20:53:50 +0300

    Created DepartmentConfiguration for configuring Department entities.

---
+++ ITS.DAL/Configuration/DepartmentConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using ITS.DAL.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ITS.DAL.Configuration
+{
+	internal class DepartmentConfiguration : IEntityTypeConfiguration<Department>
+	{
+		public void Configure(EntityTypeBuilder<Department> builder)
+		{
+			builder
+				.HasMany(d => d.Employees)
+				.WithOne(u => u.Department)
+				.HasForeignKey(u => u.DepartmentId)
+				.OnDelete(DeleteBehavior.Restrict);
+
+			builder.HasData(DataSeed.Departments);
+		}
+	}
+}
