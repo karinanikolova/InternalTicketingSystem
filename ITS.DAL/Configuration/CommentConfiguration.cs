13c56870a91ee9119509508eb2acc11233a624d9
karinanikolova <lianna_kn@yahoo.com>
2025-05-21 20:57:59 +0300

    Modified Configured method in CommentConfiguration by providing seeding data to builder.

--- ITS.DAL/Configuration/CommentConfiguration.cs
+++ ITS.DAL/Configuration/CommentConfiguration.cs
@@ -19,6 +19,8 @@ namespace ITS.DAL.Configuration
 				.WithMany(t => t.Comments)
 				.HasForeignKey(c => c.TicketId)
 				.OnDelete(DeleteBehavior.Restrict);
+
+			builder.HasData(DataSeed.Comments);
 		}
 	}
 }
