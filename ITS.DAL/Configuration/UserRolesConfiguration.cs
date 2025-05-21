3c1f49f6388e68b133d640c73bb93da1af71f0d4
karinanikolova <lianna_kn@yahoo.com>
2025-05-21 20:55:39 +0300

    Created UserRolesConfiguration for configuring IdentityUserRole<Guid> entities.

---
+++ ITS.DAL/Configuration/UserRolesConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ITS.DAL.Configuration
+{
+	internal class UserRolesConfiguration : IEntityTypeConfiguration<IdentityUserRole<Guid>>
+	{
+		public void Configure(EntityTypeBuilder<IdentityUserRole<Guid>> builder)
+		{
+			builder.ToTable("UserRoles");
+
+			builder.HasData(DataSeed.UserRoles);
+		}
+	}
+}
