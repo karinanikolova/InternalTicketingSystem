8fdcae75e23a5cd9a5522e58ee975dbbdfd45469
karinanikolova <lianna_kn@yahoo.com>
2025-05-21 20:48:22 +0300

    Created Department entity class.

---
+++ ITS.DAL/Data/Models/Department.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using static ITS.DAL.Constants.DataConstants.Department;
+
+namespace ITS.DAL.Data.Models
+{
+	public class Department
+	{
+		[Required]
+		[Comment("Department identifier")]
+		public Guid Id { get; set; }
+
+		[Required]
+		[MaxLength(DepartmentNameMaxLength)]
+		[Comment("Department name")]
+		public string Name { get; set; }
+
+		public ICollection<ApplicationUser> Employees { get; set; } = new List<ApplicationUser>();
+	}
+}
