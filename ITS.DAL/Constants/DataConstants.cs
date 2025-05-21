9859534a43d8e17ba1c0367fd4e64e860a13a267
karinanikolova <lianna_kn@yahoo.com>
2025-05-21 20:50:15 +0300

    Added more classes holding constant properties for entities in DataConstants.

--- ITS.DAL/Constants/DataConstants.cs
+++ ITS.DAL/Constants/DataConstants.cs
@@ -4,7 +4,7 @@
 	{
 		public static class Category
 		{
-			public const int NameMinLength = 3;
+			public const int NameMinLength = 1;
 			public const int NameMaxLength = 50;
 		}
 
@@ -21,6 +21,21 @@
 		{
 			public const int MessageMinLength = 1;
 			public const int MessageMaxLength = 1000;
+		}
+
+		public static class Department
+		{
+			public const int DepartmentNameMinLength = 1;
+			public const int DepartmentNameMaxLength = 50;
+		}
+
+		public static class ApplicationUser
+		{
+			public const int UserFirstNameMinLength = 1;
+			public const int UserFirstNameMaxLength = 50;
+
+			public const int UserLastNameMinLength = 1;
+			public const int UserLastNameMaxLength = 50;
 		}
 	}
 }
