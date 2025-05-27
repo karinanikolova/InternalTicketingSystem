5ee578d6ee068da142bf1b23d1ac519c40fd6d02
karinanikolova <lianna_kn@yahoo.com>
2025-05-27 21:46:36 +0300

    Created IRepository interface with methods to be implemented for operations on the AppDbContext.

---
+++ ITS.DAL/Data/Utilities/Contracts/IRepository.cs
@@ -0,0 +1,23 @@
+﻿namespace ITS.DAL.Data.Utilities.Contracts
+{
+	public interface IRepository
+	{
+		IQueryable<TEntity> All<TEntity>() where TEntity : class;
+
+		IQueryable<TEntity> AllReadOnly<TEntity>() where TEntity : class;
+
+		Task AddAsync<TEntity>(TEntity entity) where TEntity : class;
+
+		void Add<TEntity>(TEntity entity) where TEntity : class;
+
+		Task<int> SaveChangesAsync();
+
+		int SaveChanges();
+
+		void Delete<TEntity>(TEntity entityToDelete) where TEntity : class;
+
+		void DeleteRange<TEntity>(params TEntity[] entitiesToDelete) where TEntity : class;
+
+		Task<TEntity?> GetByIdAsync<TEntity>(object id) where TEntity : class;
+	}
+}
