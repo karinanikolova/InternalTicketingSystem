a5da1fef5d9741e70148dca8990f46f3ea8192c5
karinanikolova <lianna_kn@yahoo.com>
2025-05-28 18:54:22 +0300

    Created AuthenticationService implementing service methods for AuthenticationController.

---
+++ ITS.Application/Services/AuthenticationService.cs
@@ -0,0 +1,24 @@
+﻿using ITS.Core.Services.Contracts;
+using ITS.DAL.Data.Models;
+using ITS.DAL.Data.Utilities.Contracts;
+
+namespace ITS.Core.Services
+{
+	public class AuthenticationService : IAuthenticationService
+	{
+		private readonly IRepository _repository;
+
+		public AuthenticationService(IRepository repository)
+		{
+			_repository = repository;
+		}
+
+		public Task<Guid?> GetDepartmentIdByNameAsync(string departmentName)
+		{
+			var department = _repository.AllReadOnly<Department>()
+				.FirstOrDefault(d => d.Name == departmentName);
+
+			return Task.FromResult(department?.Id);
+		}
+	}
+}
