f79cb40eaab0e8132a7d88dcad86ca0ec8b584ce
karinanikolova <lianna_kn@yahoo.com>
2025-05-28 20:31:44 +0300

    Modified Program.cs file by decluttering it, using custom IServiceCollection extension methods holdling corresponsing logic.

--- ITS.Api/Program.cs
+++ ITS.Api/Program.cs
@@ -1,15 +1,3 @@
-using ITS.Core.Services;
-using ITS.Core.Services.Contracts;
-using ITS.DAL.Data;
-using ITS.DAL.Data.Models;
-using ITS.DAL.Data.Utilities.Contracts;
-using ITS.DAL.Data.Utilities;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
-
 namespace ITSApi
 {
 	public class Program
@@ -18,53 +6,14 @@ namespace ITSApi
 		{
 			var builder = WebApplication.CreateBuilder(args);
 
-			// Add services to the container.
-			builder.Services.AddDbContext<AppDbContext>(options =>
-				options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-			builder.Services.AddScoped<IRepository, Repository>();
-
-			builder.Services.AddIdentity<ApplicationUser, ApplicationRole>(options => 
-			{
-				options.User.RequireUniqueEmail = true;
-				options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
-
-				options.SignIn.RequireConfirmedAccount = false;
-
-				options.Password.RequiredLength = 6;
-				options.Password.RequireNonAlphanumeric = true;
-				options.Password.RequireDigit = true;
-				options.Password.RequireLowercase = true;
-				options.Password.RequireUppercase = true;
-			})
-				.AddEntityFrameworkStores<AppDbContext>()
-				.AddDefaultTokenProviders();
-
-			builder.Services.AddAuthentication(options =>
-			{
-				options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-				options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-			})
-			.AddJwtBearer(options =>
-			{
-				options.TokenValidationParameters = new TokenValidationParameters
-				{
-					ValidateIssuer = true,
-					ValidateAudience = true,
-					ValidateLifetime = true,
-					ValidateIssuerSigningKey = true,
-					ValidIssuer = builder.Configuration["Jwt:Issuer"],
-					ValidAudience = builder.Configuration["Jwt:Audience"],
-					IssuerSigningKey = new SymmetricSecurityKey(
-						Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
-				};
-			});
-
+			builder.Services.AddApplicationDbContext(builder.Configuration);
+			builder.Services.AddApplicationIdentity(builder.Configuration);
+			builder.Services.AddApplicationAuthentication(builder.Configuration);
 			builder.Services.AddAuthorization();
 
-			builder.Services.AddControllers();
+			builder.Services.AddApplicationServices();
 
-			builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+			builder.Services.AddControllers();
 
 			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 			builder.Services.AddEndpointsApiExplorer();
