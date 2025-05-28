25cc0ce238feb3235fa90aaace53e52a0fd38bb1
karinanikolova <lianna_kn@yahoo.com>
2025-05-28 18:46:06 +0300

    Added new parameter to GenerateToken method in JwtGenerator static class.

--- ITS.Api/Configuration/JwtGenerator.cs
+++ ITS.Api/Configuration/JwtGenerator.cs
@@ -8,7 +8,7 @@ namespace ITS.Api.Configuration
 {
 	public static class JwtGenerator
 	{
-		public static string GenerateToken(ApplicationUser user, IList<string> roles, IConfiguration configuration)
+		public static string GenerateToken(ApplicationUser user, IList<string> roles, IConfiguration configuration, bool rememberMe)
 		{
 			var claims = new List<Claim>()
 			{
@@ -24,12 +24,13 @@ namespace ITS.Api.Configuration
 
 			var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]));
 			var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+			var tokenExpiration = rememberMe ? DateTime.Now.AddDays(7) : DateTime.Now.AddHours(1);
 
 			var token = new JwtSecurityToken(
 				issuer: configuration["Jwt:Issuer"],
 				audience: configuration["Jwt:Audience"],
 				claims: claims,
-				expires: DateTime.Now.AddHours(1),
+				expires: tokenExpiration,
 				signingCredentials: credentials);
 
 			return new JwtSecurityTokenHandler().WriteToken(token);
