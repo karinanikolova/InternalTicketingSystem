db50d3337f5e85b8e417550243cfb0ac288688e8
karinanikolova <lianna_kn@yahoo.com>
2025-05-28 18:54:41 +0300

    Created LoginDto.

---
+++ ITS.Application/Models/Authentication/LoginDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static ITS.DAL.Constants.DataConstants.ApplicationUser;
+using static ITS.DAL.Constants.MessageConstants;
+
+namespace ITS.Core.Models.Authentication
+{
+	public class LoginDto
+	{
+		[Required]
+		[StringLength(UserEmailMaxLength, MinimumLength = UserNameMinLength, ErrorMessage = FieldLengthMessage)]
+		[Display(Name = "Email or username")]
+		public string UserNameOrEmail { get; set; }
+
+		[Required]
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
+
+		[Display(Name = "Remember me")]
+		public bool RememberMe { get; set; }
+	}
+}
