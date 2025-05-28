6f5f5f00274ef54cb8edd61f911929c972655237
karinanikolova <lianna_kn@yahoo.com>
2025-05-28 19:00:18 +0300

    Created MessageConstants static class.

---
+++ ITS.DAL/Constants/MessageConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace ITS.DAL.Constants
+{
+	public static class MessageConstants
+	{
+		public const string FieldLengthMessage = "The field {0} must contain between {2} and {1} characters";
+
+		public const string InvalidEmailMessage = "Invalid email address";
+
+		public const string FailedPasswordComparisonMessage = "The password and confirmation password do not match";
+	}
+}
