544205828f3d8aec7c3e8f5a5a7a92cf4f4a6982
karinanikolova <lianna_kn@yahoo.com>
2025-05-29 19:51:14 +0300

    Created CommentDto.

---
+++ ITS.Application/Models/Comment/CommentDto.cs
@@ -0,0 +1,14 @@
+﻿namespace ITS.Core.Models.Comment
+{
+	public class CommentDto
+	{
+		public Guid Id { get; set; }
+		public string Message { get; set; }
+
+		public DateTime CreatedOn { get; set; }
+
+		public Guid TicketId { get; set; }
+
+		public Guid CreatorId { get; set; }
+	}
+}
