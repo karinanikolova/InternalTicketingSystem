2eebbcefc5846ceb4fc12fa1d1e6426e27024adb
karinanikolova <lianna_kn@yahoo.com>
2025-05-29 19:54:03 +0300

    Added TicketService to the IoC container.

--- ITS.Api/Extensions/ServiceCollectionExtension.cs
+++ ITS.Api/Extensions/ServiceCollectionExtension.cs
@@ -72,6 +72,7 @@ namespace Microsoft.Extensions.DependencyInjection
 		public static IServiceCollection AddApplicationServices(this IServiceCollection services)
 		{
 			services.AddScoped<IAuthenticationService, AuthenticationService>();
+			services.AddScoped<ITicketService, TicketService>();
 
 			return services;
 		}
