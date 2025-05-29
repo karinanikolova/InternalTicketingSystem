29e92fd6dee0d094b4650889e82274a21a3fbde0
karinanikolova <lianna_kn@yahoo.com>
2025-05-29 19:51:56 +0300

    Created ITicketService.

---
+++ ITS.Application/Services/Contracts/ITicketService.cs
@@ -0,0 +1,11 @@
+﻿using ITS.Core.Models.Ticket;
+
+namespace ITS.Core.Services.Contracts
+{
+	public interface ITicketService
+	{
+		Task<IEnumerable<TicketDto>> GetAllTicketsAsync();
+
+		Task<TicketDto?> GetTicketByIdAsync(Guid ticketId);
+	}
+}
