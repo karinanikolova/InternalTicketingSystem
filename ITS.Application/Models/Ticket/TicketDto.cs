6d4605aace92ac972e96d3f48956801f8f0f2be8
karinanikolova <lianna_kn@yahoo.com>
2025-05-29 19:51:30 +0300

    Created TicketDto.

---
+++ ITS.Application/Models/Ticket/TicketDto.cs
@@ -0,0 +1,29 @@
+﻿using ITS.Core.Models.Comment;
+using ITS.DAL.Enums;
+
+namespace ITS.Core.Models.Ticket
+{
+	public class TicketDto
+	{
+		public Guid Id { get; set; }
+		public string Title { get; set; }
+
+		public string Description { get; set; }
+
+		public Guid CategoryId { get; set; }
+
+		public Status Status { get; set; }
+
+		public Priority Priority { get; set; }
+
+		public DateTime CreatedOn { get; set; }
+
+		public DateTime? DueDate { get; set; }
+
+		public Guid CreatorId { get; set; }
+
+		public Guid AssignedToUserId { get; set; }
+
+		public ICollection<CommentDto> Comments { get; set; } = new List<CommentDto>();
+	}
+}
