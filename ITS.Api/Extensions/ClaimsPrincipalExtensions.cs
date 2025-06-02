c10b54cafdd495130066e00122367238ef5fb0e1
karinanikolova <lianna_kn@yahoo.com>
2025-06-02 18:14:53 +0300

    Created ClaimsPrincipalExtensions holding custom methods for the user identity.

---
+++ ITS.Api/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,20 @@
+﻿using static ITS.DAL.Constants.CustomClaims;
+using static ITS.DAL.Constants.CustomRoles;
+
+namespace System.Security.Claims
+{
+	public static class ClaimsPrincipalExtensions
+	{
+		public static string UserId(this ClaimsPrincipal user)
+			=> user.FindFirstValue(ClaimTypes.NameIdentifier)!;
+
+		public static string FindUserRole(this ClaimsPrincipal user)
+			=> user.FindFirstValue(ClaimTypes.Role)!;
+
+		public static bool IsAdmin(this ClaimsPrincipal user)
+			=> user.IsInRole(AdminRole);
+
+		public static bool IsSupportAgent(this ClaimsPrincipal user)
+			=> user.HasClaim(c => c.Type == UserSupportAgentClaim && c.Value == "true");
+	}
+}
