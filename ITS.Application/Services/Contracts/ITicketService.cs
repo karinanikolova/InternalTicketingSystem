db58c2cad68d9f9bf533ef46431ea2b9d31df78b
karinanikolova <lianna_kn@yahoo.com>
2025-06-04 21:24:11 +0300

    Added DoesStatusExist and DoesPriorityExist methods to ITicketService.

--- ITS.Application/Services/Contracts/ITicketService.cs
+++ ITS.Application/Services/Contracts/ITicketService.cs
@@ -4,10 +4,14 @@ namespace ITS.Core.Services.Contracts
 {
 	public interface ITicketService
 	{
-		Task<IEnumerable<TicketViewDto>> GetAllTicketsAsync(string userRole, bool isSupportAgent, string userId);
+		Task<IEnumerable<TicketViewDto>> GetAllTicketsAsync(string userRole, bool isSupportAgent, Guid? userId);
 
 		Task<TicketViewDto?> GetTicketByIdAsync(Guid ticketId);
 
-		Task CreateTicketAsync(TicketCreateDto ticketFormDto);
+		Task CreateTicketAsync(TicketCreateDto ticketFormDto, Guid creatorId, Guid adminUserId);
+
+		bool DoesStatusExist(int statusId);
+
+		bool DoesPriorityExist(int priorityId);
 	}
 }
