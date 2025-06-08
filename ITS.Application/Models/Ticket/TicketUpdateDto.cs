3188a4513c388c3faec3b4984808aa7541c31561
karinanikolova <lianna_kn@yahoo.com>
2025-06-08 20:48:05 +0300

    Created TicketUpdateDto.

---
+++ ITS.Application/Models/Ticket/TicketUpdateDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static ITS.DAL.Constants.MessageConstants;
+
+namespace ITS.Core.Models.Ticket
+{
+	public class TicketUpdateDto : TicketCreateDto
+	{
+		public Guid CreatorId { get; set; }
+
+		[Required(ErrorMessage = RequiredFieldMessage)]
+		[Display(Name = "Assigned to user")]
+		public Guid AssignedToUserId { get; set; }
+	}
+}
