6accc744cc3dd0891f3f8a488d74f0fff6bad31e
karinanikolova <lianna_kn@yahoo.com>
2025-06-11 17:11:09 +0300

    Created a custom attribute for date validation called FutureDateAttribute.

---
+++ ITS.Application/Validation/FutureDateAttribute.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ITS.Core.Validation
+{
+	public class FutureDateAttribute : ValidationAttribute
+	{
+		public FutureDateAttribute()
+			: base("The date must be today or in the future.")
+		{
+
+		}
+
+		public FutureDateAttribute(string errorMessage)
+			: base(errorMessage)
+		{
+
+		}
+
+		protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+		{
+			if (value == null)
+			{
+				return ValidationResult.Success;
+			}
+
+			if (value is DateTime dateValue)
+			{
+				if (dateValue.Date >= DateTime.Today)
+				{
+					return ValidationResult.Success;
+				}
+			}
+
+			return new ValidationResult(ErrorMessage = "The specified date is not valid.");
+		}
+	}
+}
