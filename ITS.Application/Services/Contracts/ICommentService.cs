b6d30efd84d61cae3f3d53546dc708d6b90b3757
karinanikolova <lianna_kn@yahoo.com>
2025-06-17 17:47:23 +0300

    Created ICommentService.

---
+++ ITS.Application/Services/Contracts/ICommentService.cs
@@ -0,0 +1,9 @@
+﻿using ITS.Core.Models.Comment;
+
+namespace ITS.Core.Services.Contracts
+{
+	public interface ICommentService
+	{
+		Task CreateCommentAsync(Guid ticketId, CommentCreateDto commentDto, Guid userId);
+	}
+}
