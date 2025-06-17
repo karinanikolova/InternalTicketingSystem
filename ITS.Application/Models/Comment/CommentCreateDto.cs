f22f1dd1e025134ba970b8346944d63d62d268a8
karinanikolova <lianna_kn@yahoo.com>
2025-06-17 17:47:00 +0300

    Added CommentCreateDto.

---
+++ ITS.Application/Models/Comment/CommentCreateDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static ITS.DAL.Constants.DataConstants.Comment;
+using static ITS.DAL.Constants.MessageConstants;
+
+namespace ITS.Core.Models.Comment
+{
+	public class CommentCreateDto
+	{
+		[Required(ErrorMessage = RequiredFieldMessage)]
+		[StringLength(MessageMaxLength, MinimumLength = MessageMinLength, ErrorMessage = FieldLengthMessage)]
+		public string Message { get; set; }
+	}
+}
