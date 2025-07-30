98ecf3f34802794ad587bfc705436bd2cd6cdec0
karinanikolova <lianna_kn@yahoo.com>
2025-07-30 16:33:26 +0300

    Added new methods to ICommentService.

--- ITS.Application/Services/Contracts/ICommentService.cs
+++ ITS.Application/Services/Contracts/ICommentService.cs
@@ -5,5 +5,11 @@ namespace ITS.Core.Services.Contracts
 	public interface ICommentService
 	{
 		Task CreateCommentAsync(Guid ticketId, CommentCreateDto commentDto, Guid userId);
+
+		Task<CommentViewDto?> GetCommentByIdAsync(Guid commentId);
+
+		Task UpdateCommentAsync(Guid commentId, CommentUpdateDto commentDto);
+
+		Task DeleteCommentAsync(Guid commentId);
 	}
 }
